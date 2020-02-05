a85c92971c48393fbab17764d4c556a5b2cb6faa
LeonYG <2745210147@qq.com>
2020-02-05 13:53:19 +0800

    2

---
+++ hello/target/debug/deps/hello.d
@@ -0,0 +1,5 @@
+C:\Users\LeonYG\GitSrc\newRustTest\hello\target\debug\deps\hello.exe: src\main.rs
+
+C:\Users\LeonYG\GitSrc\newRustTest\hello\target\debug\deps\hello.d: src\main.rs
+
+src\main.rs:
---
+++ hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+C:\Users\LeonYG\GitSrc\newRustTest\hello\target\debug\hello.exe: C:\Users\LeonYG\GitSrc\newRustTest\hello\src\main.rs
