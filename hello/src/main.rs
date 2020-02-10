3264cbe50e2ed646c7d7214792bf7c0a37f35aa7
LeonYG <2745210147@qq.com>
2020-02-10 14:25:54 +0800

    stdin test

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,4 +1,8 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
-    
+    //println!("Hello, world!");
+    println!("please enter a guess");
+    let mut guess = String::new();//.expect("new string creat fail");
+    io::stdin().read_line(&mut guess).expect("get a guess from stdin faild");
+    println!("guess:{}",guess);
 }
