8825d6a6e11e6417aacf1237543cad68d9ddfa95
LeonYG <2745210147@qq.com>
2020-02-12 16:50:39 +0800

    done

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -4,20 +4,33 @@ use rand::Rng;
 fn main() {
     /*#1 生成随机数*/
     let secret_num = rand::thread_rng().gen_range(1,101);
-    println!("secret_num is:{}",secret_num);
-    /*#2 获取命令行字符串*/
-    println!("please enter a guess");
-    let mut guess = String::new();
-    stdin().read_line(&mut guess).expect("get a guess from stdin faild");
-    println!("guess:{}",guess);
-    /*#3 字符串->数字 类型强转*/
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
-    /*#4 对象的cmp接口返回排序器库中的排序结果枚举类型*/
-    println!("comparing...");
-    match guess.cmp(&secret_num) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+    loop{
+        println!("secret_num is:{}",secret_num);
+        /*#2 获取命令行字符串*/
+        println!("please enter a guess");
+        let mut guess = String::new();
+        stdin().read_line(&mut guess).expect("get a guess from stdin faild");
+        println!("guess:{}",guess);
+        /*#3 字符串->数字 类型强转*/
+        /*#5 异常处理：注意对返回值的抓取*/
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => {
+                println!("input not a num");
+                continue;
+            }
+        };
+        /*#4 对象的cmp接口返回排序器库中的排序结果枚举类型*/
+        println!("comparing...");
+        match guess.cmp(&secret_num) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
         }
+    }
+    
 
 }
